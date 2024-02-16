4500a736098e96f00a569e0fe7e740d2e78942e3
Thomas <thomasthanos28@gmail.com>
2024-02-16 19:51:18 +0200

    Update install_apps.bat

--- install_apps.bat
+++ install_apps.bat
@@ -1,31 +1,4 @@
 @echo off
-
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params = %*:"=""
-    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
-    pushd "%CD%"
-    CD /D "%~dp0"
-:--------------------------------------
-
-@echo off
 color 3
 echo.
 echo  Kanei Update oles tis efarmoges!
@@ -41,7 +14,6 @@ echo      ///            ///       ///
 echo      ///            ///       ///
 echo.
 echo.
-pause
 
 @echo off
 reg add "HKEY_CURRENT_USER\Console" /v "FaceName" /t REG_SZ /d "Cascadia Mono" /f
