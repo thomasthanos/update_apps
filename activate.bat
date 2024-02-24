54325b1c905df1d2032db90cb118ff3519aec46b
Thomas <thomasthanos28@gmail.com>
2024-02-24 18:12:54 +0200

    Add files via upload

---
+++ activate.bat
@@ -0,0 +1,23 @@
+echo off
+echo. 
+
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+pause
+slmgr /skms kms9.MSGuides.com
+pause
+slmgr /ato
+pause
+echo.
+echo.
+echo.
+echo.
+echo  ============       ///       ///
+echo      ///            ///       ///  
+echo      ///            ///       /// 
+echo      ///            /// ///// ///
+echo      ///            ///       ///  
+echo      ///            ///       ///
+echo      ///            ///       ///
+echo.
+echo.
+pause
