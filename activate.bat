a702db00e4c15fda3a859282c28f1f7105849adf
Thomas <thomasthanos28@gmail.com>
2024-02-26 11:24:32 +0200

    Update activate.bat

--- activate.bat
+++ activate.bat
@@ -5,10 +5,13 @@ echo ACTIVATE WINDOWS
 color a
 slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
 timeout /t 2 >nul
+echo ACTIVATE WINDOWS
 slmgr /skms kms9.MSGuides.com
 timeout /t 2 >nul
+echo ACTIVATE WINDOWS
 slmgr /ato
 timeout /t 2 >nul
+echo F     FINISH
 echo.
 echo.
 echo.
