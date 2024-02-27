41557e02ef5722245658c4278f8024228fba39a7
Thomas <thomasthanos28@gmail.com>
2024-02-27 18:32:46 +0200

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -1,21 +1,4 @@
 @echo off
-color 3
-echo.
-echo.
-echo  ============       ///       ///
-echo      ///            ///       ///  
-echo      ///            ///       /// 
-echo      ///            /// ///// ///
-echo      ///            ///       ///  
-echo      ///            ///       ///
-echo      ///            ///       ///
-echo.
-echo.
-echo  Kanei Update oles tis efarmoges!
-echo  _________________________________
-echo.
-
-@echo off
 reg add "HKEY_CURRENT_USER\Console" /v "FaceName" /t REG_SZ /d "Cascadia Mono" /f
 reg add "HKEY_CURRENT_USER\Console" /v "FontFamily" /t REG_DWORD /d 0x00000036 /f
 reg add "HKEY_CURRENT_USER\Console" /v "FontSize" /t REG_DWORD /d 0x00140000 /f
@@ -23,7 +6,7 @@ reg add "HKEY_CURRENT_USER\Console" /v "FontWeight" /t REG_DWORD /d 0x00000990 /
 cls
 
 @echo off 
-color 3
+color 2F  REM πράσινο φόντο και λευκό προσκήνιο
 echo.
 echo.
 echo  ============       ///       ///
