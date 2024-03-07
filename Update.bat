eacebb818e8e37d9da76a18ead9178831654961e
Thomas <thomasthanos28@gmail.com>
2024-03-07 17:13:47 +0200

    Update Update.bat

--- Update.bat
+++ Update.bat
@@ -1,10 +1,3 @@
-@echo off
-reg add "HKEY_CURRENT_USER\Console" /v "FaceName" /t REG_SZ /d "Cascadia Mono" /f
-reg add "HKEY_CURRENT_USER\Console" /v "FontFamily" /t REG_DWORD /d 0x00000036 /f
-reg add "HKEY_CURRENT_USER\Console" /v "FontSize" /t REG_DWORD /d 0x00140000 /f
-reg add "HKEY_CURRENT_USER\Console" /v "FontWeight" /t REG_DWORD /d 0x00000990 /f
-cls
-
 @echo off 
 color 3
 echo.
